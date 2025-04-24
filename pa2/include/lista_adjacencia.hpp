ffdd06173f50b43501ec12298446a6be0b1e85e3
andreassisrg <andreassisrg@gmail.com>
2025-04-24 19:49:35 -0300

    feat: initial structure practice 2

---
+++ pa2/include/lista_adjacencia.hpp
@@ -0,0 +1,13 @@
+#ifndef LISTA_ADJACENCIA_HPP
+#define LISTA_ADJACENCIA_HPP
+
+class ListaAdjacencia {
+    public:
+        ListaAdjacencia();
+        ~ListaAdjacencia();
+
+    private:
+    
+};
+
+#endif
---
+++ pa2/src/lista_adjacencia.cpp
@@ -0,0 +1,2 @@
+#include "../include/lista_adjacencia.hpp"
+
