3bb53323f7871c2f79311cb247eb0e0cb7b7707d
andreassisrg <andreassisrg@gmail.com>
2025-05-02 12:03:45 -0300

    feat: completed pa2 7/7

--- pa2/src/graph.cpp
+++ pa2/src/graph.cpp
@@ -1,2 +1,41 @@
 #include "../include/graph.hpp"
 
+Grafo::Grafo() {}
+
+
+Grafo::~Grafo() {}
+
+
+void Grafo::InsereAresta(int v, int w) {
+    vertices.InsereAresta(v, w);
+}
+
+
+void Grafo::InsereVertice() {
+    vertices.InsereVertice();
+}
+
+
+int Grafo::QuantidadeVertices() {
+    return vertices.QuantidadeVertices();
+}
+
+
+int Grafo::QuantidadeArestas() {
+    return vertices.QuantidadeArestas();
+}
+
+
+int Grafo::GrauMinimo() {
+    return vertices.GrauMinimo();
+}
+
+
+int Grafo::GrauMaximo() {
+    return vertices.GrauMaximo();
+}
+
+
+void Grafo::ImprimeVizinhos(int v) {
+    vertices.ImprimeVizinhos(v);
+}
--- pa2/src/lista_adjacencia.cpp
+++ pa2/src/lista_adjacencia.cpp
@@ -1,2 +1,83 @@
 #include "../include/lista_adjacencia.hpp"
+#include <iostream>
 
+ListaAdjacencia::ListaAdjacencia() {
+    tamanho = 0;
+    listaVertices = new ListaEncadeada<Vertice>();
+}
+
+
+void ListaAdjacencia::InsereVertice() {
+    Vertice v;
+    listaVertices->InsereFinal(v);
+    tamanho++;
+}
+
+
+Vertice* ListaAdjacencia::getVertice(int indice) {
+    if (indice < 0 || indice >= tamanho) return nullptr;
+    return &((*listaVertices)[indice]);
+}
+
+
+void ListaAdjacencia::InsereAresta(int v, int w) {
+    Vertice* V = getVertice(v);
+    if (!V) return;
+    V->vizinhos.InsereFinal(w);
+}
+
+
+int ListaAdjacencia::QuantidadeVertices() {
+    return tamanho;
+}
+
+
+int ListaAdjacencia::QuantidadeArestas() {
+    int soma = 0;
+    for (int i = 0; i < tamanho; ++i) {
+        Vertice* V = getVertice(i);
+        soma += V->vizinhos.Tamanho();
+    }
+    return soma / 2;
+}
+
+
+int ListaAdjacencia::GrauMinimo() {
+    if (tamanho == 0) return 0;
+    int minG = getVertice(0)->vizinhos.Tamanho();
+    for (int i = 1; i < tamanho; ++i) {
+        int g = getVertice(i)->vizinhos.Tamanho();
+        if (g < minG) minG = g;
+    }
+    return minG;
+}
+
+
+int ListaAdjacencia::GrauMaximo() {
+    int maxG = 0;
+    for (int i = 0; i < tamanho; ++i) {
+        int g = getVertice(i)->vizinhos.Tamanho();
+        if (g > maxG) maxG = g;
+    }
+    return maxG;
+}
+
+
+void ListaAdjacencia::ImprimeVizinhos(int v) {
+    Vertice* V = getVertice(v);
+    if (!V) {
+        std::cout << "Vértice inválido\n";
+        return;
+    }
+    Nodo<int>* aux = V->vizinhos.GetCabeca();
+    while (aux) {
+        std::cout << aux->valor << " ";
+        aux = aux->proximo;
+    }
+    std::cout << "\n";
+}
+
+
+ListaAdjacencia::~ListaAdjacencia() {
+    delete listaVertices;
+}
