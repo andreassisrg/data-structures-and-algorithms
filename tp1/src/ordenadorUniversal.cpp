659e5c261efb32ef453b136323bb69c9ec172efd
andreassisrg <andreassisrg@gmail.com>
2025-05-19 21:14:04 -0300

    feat: tp1 100/100

--- tp1/src/ordenadorUniversal.cpp
+++ tp1/src/ordenadorUniversal.cpp
@@ -29,12 +29,9 @@ OrdenadorUniversal::~OrdenadorUniversal() {}
 
 void OrdenadorUniversal::run() {
     int totalBreaks = countBreaks(V_base, size);
-    std::cout << "size " << size << " seed " << seed << " breaks " << totalBreaks << "\n" << std::endl;
+    std::cout << "size " << size << " seed " << seed << " breaks " << totalBreaks << std::endl;
     
     limiarParticao = determinaLimiarParticao(V_base);
-
-    sortperf_t s;
-    quickSort3Ins(V_base, 0, size - 1, 50, &s);
     limiarQuebras = determinaLimiarQuebras(V_base);
 }
 
@@ -53,8 +50,9 @@ int OrdenadorUniversal::determinaLimiarParticao(int *V) {
     int indiceMinCusto, indiceMaxCusto;
     double custoMPS, minCusto, maxCusto;
 
-    std::cout << std::fixed << std::setprecision(6);
     do {
+        std::cout << "\n";
+        std::cout << std::fixed << std::setprecision(9);
         // Inicialização do bloco
         std::cout << "iter " << iter++ << std::endl;
         numMPS = 0;
@@ -122,10 +120,12 @@ int OrdenadorUniversal::determinaLimiarParticao(int *V) {
         passoMPS = (int) (maxMPS - minMPS) / 5;
         if (passoMPS == 0) passoMPS++;
 
+        // Impressão das estatísticas do bloco
+        std::cout << std::fixed << std::setprecision(6);
         std::cout << "nummps "       << numMPS 
                   << " limParticao " << limParticao
                   << " mpsdiff "     << diffCusto
-                  << "\n"            << std::endl;
+                  << std::endl;
         
     } while (diffCusto > limiarCusto && numMPS >= 5);
     
@@ -133,64 +133,115 @@ int OrdenadorUniversal::determinaLimiarParticao(int *V) {
 }
 
 int OrdenadorUniversal::determinaLimiarQuebras(int *V) {
+    // Inicializando variáveis que vão ser refinadas conforme as iterações
     int minLQ = 1;
     int maxLQ = size / 2;
     int passoLQ = (int) (maxLQ - minLQ) / 5;
     int limQuebras;
-    int lq;
+    int numLQ;
     float diffCusto;
 
-    int iter = 0;
-    int numLQ;
+    // Variáveis auxiliares para cada iteração
     sortperf_t s;
-
-    // Cria um valor controlado de quebras para o vetor
-    srand48(seed);
-    int numShuffle = rand() % size;
-    shuffleVector(V_base, size, numShuffle);
+    int iter = 0;
+    int indiceMinDiff;
+    double custoLQ, minDiff;
     
     do {
+        std::cout << std::fixed << std::setprecision(9);
+        std::cout << "\n";
         // Inicialização do bloco
         std::cout << "iter " << iter++ << std::endl; 
-        limQuebras = minLQ;
         numLQ = 0;
+        limQuebras = minLQ;
+        minDiff = std::numeric_limits<int>::max();
+        indiceMinDiff = 0;
+        double custoQuicksort[10];
+        double custoInsertionSort[10];
+        int lq[10];
 
         for (int t = minLQ; t <= maxLQ; t += passoLQ) {
+            lq[numLQ] = t;
+
+            // É necessário ordenador o vetor antes de fazer qualquer operação nele
+            quickSort3Ins(V_base, 0, size - 1, limiarParticao, &s);
+            
+            // Criar um valor controlado de quebras para o vetor previamente ordenado
+            srand48(seed);
+            shuffleVector(V_base, size, t);
+
             // Criar vetores auxiliares para chamada do QuickSort e InsertionSort
             int *Vq = new int[size];
             int *Vi = new int[size];
             memcpy(Vq, V_base, size * sizeof(int));
             memcpy(Vi, V_base, size * sizeof(int));
 
+            // Rodar quicksort, pegar estatísticas e imprimir linha+
             resetcounter(&s);
-            quickSort3Ins(Vq, );
+            quickSort3Ins(Vq, 0, size - 1, limiarParticao, &s);
+            custoLQ = cost(&s, a, b, c);
+            custoQuicksort[numLQ] = custoLQ;
+            std::cout << "qs "
+                      << "lq "     << t
+                      << " cost "  << custoLQ
+                      << " cmp "   << s.cmp
+                      << " move "  << s.move
+                      << " calls " << s.calls
+                      << std::endl;
+
+            // Rodar insertionsort, pegar estatísticas e imprimir linha
             resetcounter(&s);
-            insertionSort(Vi, );
+            insertionSort(Vi, 0, size - 1, &s);
+            custoLQ = cost(&s, a, b, c);
+            custoInsertionSort[numLQ] = custoLQ;
+            std::cout << "in "
+                      << "lq "     << t
+                      << " cost "  << custoLQ
+                      << " cmp "   << s.cmp
+                      << " move "  << s.move
+                      << " calls " << s.calls
+                      << std::endl;
+
+            // Determinar o índice para o qual o quicksort tem custo mais similar ao
+            // insertionsort
+            double lqDiff = fabs(custoInsertionSort[numLQ] - custoQuicksort[numLQ]);
+            if (lqDiff < minDiff) {
+                minDiff = lqDiff;
+                indiceMinDiff = numLQ;
+                limQuebras = t;
+            }
 
             delete[] Vq, Vi;
             numLQ++;
         }
 
         // Refinação com base nos índices
-        if (indiceMinCusto == 0) {
-            minLQ = mps[0];
-            maxLQ = mps[2];
-            diffCusto = fabs(custo[0] - custo[2]);
-        } else if (indiceMinCusto == numMPS - 1) {
-            minMPS = mps[numMPS - 3];
-            maxMPS = mps[numMPS - 1];
-            diffCusto = fabs(custo[numMPS - 3] - custo[numMPS -1]);
+        if (indiceMinDiff == 0) {
+            minLQ = lq[0];
+            maxLQ = lq[2];
+            diffCusto = fabs(custoInsertionSort[0] - custoInsertionSort[2]);
+        } else if (indiceMinDiff == numLQ - 1) {
+            minLQ = lq[numLQ - 3];
+            maxLQ = lq[numLQ - 1];
+            diffCusto = fabs(custoInsertionSort[numLQ - 3] - custoInsertionSort[numLQ -1]);
         } else {
-            minMPS = mps[indiceMinCusto - 1];
-            maxMPS = mps[indiceMinCusto + 1];
-            diffCusto = fabs(custo[indiceMinCusto - 1] - custo[indiceMinCusto + 1]);
+            minLQ = lq[indiceMinDiff - 1];
+            maxLQ = lq[indiceMinDiff + 1];
+            diffCusto = fabs(custoInsertionSort[indiceMinDiff - 1] - custoInsertionSort[indiceMinDiff + 1]);
         }
 
         // Utiliza os novos máximos e mínimos para criar a nova faixa
-        passoMPS = (int) (maxMPS - minMPS) / 5;
-        if (passoMPS == 0) passoMPS++;
+        passoLQ = (int) (maxLQ - minLQ) / 5;
+        if (passoLQ == 0) passoLQ++;
 
-    } while (diffCusto > limiarCusto && numlq >= 6);
+        // Impressão das estatísticas do bloco
+        std::cout << std::fixed << std::setprecision(6);
+        std::cout << "numlq "        << numLQ
+                  << " limQuebras "  << limQuebras
+                  << " lqdiff "      << diffCusto
+                  << std::endl;
+
+    } while (diffCusto > limiarCusto && numLQ >= 5);
 
     return limQuebras;
 }
